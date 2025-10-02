6715c9bd2879e316da8d0e35319f39730778085a
cj404-proj <normal.mymail@gmail.com>
2025-10-02 12:14:52 +0530

    Modified the terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "tfstate-rg"
     storage_account_name = "newtfstorageacc"
-    container_name = "tfstatecontainer"
+    container_name = "tfstate"
     key = "infra.tfstate"
     
   }
